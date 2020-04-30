70bc3bee98d80f53306502d3f2ca7dc0c9cf6ba4
chisms <64498665+chisms@users.noreply.github.com>
2020-04-30 13:04:40 -0500

    Main-Comments
    Documentation

--- Main.java
+++ Main.java
@@ -10,9 +10,13 @@ public class Main {
     public static void main(String[] args) {
 	    Scanner scanner = new Scanner(System.in);
 
+	// Initializes and populate sthe hashmap with members from the location class.
+	    
         Map<String, Integer> tempExit = new HashMap<String, Integer>();
         locations.put(0, new Location(0, "You are sitting in front of a computer learning Java", tempExit));
 
+	// Adds directional controls to the game    
+	
         tempExit = new HashMap<String, Integer>();
         tempExit.put("W", 2);
         tempExit.put("E", 3);
@@ -21,6 +25,8 @@ public class Main {
         //tempExit.put("Q", 0);
         locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building",tempExit));
 
+	// Adds specific location instances to the hash map    
+	
         tempExit = new HashMap<String, Integer>();
         tempExit.put("N", 5);
         //tempExit.put("Q", 0);
@@ -44,6 +50,8 @@ public class Main {
         locations.put(5, new Location(5, "You are in the forest",tempExit));
 
 
+	// Adds directional controls to a seperate map, allowing for transitioning between locations    
+	    
         Map<String, String> vocabulary = new HashMap<String, String>();
         vocabulary.put("QUIT", "Q");
         vocabulary.put("NORTH", "N");
@@ -52,6 +60,8 @@ public class Main {
         vocabulary.put("WEST", "W");
 
         int loc = 1;
+	    
+	// Infinite while loop, program will end when user quits or reaches the predetermined exit.    
         while(true) {
             System.out.println(locations.get(loc).getDescription());
             tempExit.remove("S");
@@ -83,16 +93,5 @@ public class Main {
                 System.out.println("You cannot go in that direction");
             }
         }
-//        String[] road = "You are standing at the end of a road before a small brick building".split(" ");
-//        for(String i : road) {
-//            System.out.println(i);
-//        }
-//
-//        System.out.println("----------------------------------------------------------");
-//
-//        String[] building = "You are inside a building, a well house for a small spring".split(", ");
-//        for(String i : building) {
-//            System.out.println(i);
-//        }
     }
 }
